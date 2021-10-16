73510c0d3d6a353d9588d6abcb57d36c8c3dc6bf
Kosuke Kihara <41236712+KOHSUK@users.noreply.github.com>
2021-10-16 11:24:49 +0900

    add realtime (#4)

--- src/hooks/use-data-loader.tsx
+++ src/hooks/use-data-loader.tsx
@@ -1,9 +1,23 @@
-import { useEffect } from 'react';
+import { useCallback, useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { authUserSelector } from 'redux/auth/selector';
-import { fetchChannels } from 'redux/channels/slice';
-import { fetchMessages } from 'redux/messages/slice';
+import { channelsSlice, fetchChannels } from 'redux/channels/slice';
+import { fetchMessages, Message, messagesSlice } from 'redux/messages/slice';
 import { fetchUser } from 'redux/user/slice';
+import { supabase } from 'services/supabase';
+import { definitions } from 'types/supabase';
+
+export const fetchUserById = async (userId: string) => {
+  try {
+    let { body } = await supabase
+      .from<definitions['users']>('users')
+      .select(`*`)
+      .eq('id', userId);
+    return body && body.length > 0 ? body[0] : null;
+  } catch (error) {
+    console.log('error', error);
+  }
+};
 
 export const useDataLoader = (props: { channelId: string }) => {
   const dispatch = useDispatch();
@@ -11,6 +25,40 @@ export const useDataLoader = (props: { channelId: string }) => {
   useEffect(() => {
     dispatch(fetchChannels());
   }, [dispatch]);
+
+  const handleNewMessage = useCallback(
+    async (newMessage: definitions['messages']) => {
+      const user = await fetchUserById(newMessage.user_id);
+      const message: Message = {
+        ...newMessage,
+        author: user || null,
+      };
+      dispatch(messagesSlice.actions.messageAddedOrUpdated(message));
+    },
+    [dispatch]
+  );
+
+  const handleNewChannel = useCallback(
+    (channel: definitions['channels']) => {
+      dispatch(channelsSlice.actions.channelAddedOrUpdated(channel));
+    },
+    [dispatch]
+  );
+
+  useEffect(() => {
+    const messageListener = supabase
+      .from<definitions['messages']>('messages')
+      .on('INSERT', (payload) => handleNewMessage(payload.new))
+      .subscribe();
+    const channelListener = supabase
+      .from<definitions['channels']>('channels')
+      .on('INSERT', (payload) => handleNewChannel(payload.new))
+      .subscribe();
+    return () => {
+      messageListener.unsubscribe();
+      channelListener.unsubscribe();
+    };
+  }, [handleNewChannel, handleNewMessage]);
 
   useEffect(() => {
     const channelNum = parseInt(props.channelId);
